5aa53b1291be6dc9deba291ff180943f7f072d6d
fogelgit1234 <94507261+fogelgit1234@users.noreply.github.com>
2021-11-17 06:18:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
             box-sizing: border-box;
         }
 
-        /* Add padding to containers */
+  
         .container {
             padding: 16px;
             background-color: white;
         }
 
-        /* Full-width input fields */
+     
         input[type=text], input[type=password] {
             width: 100%;
             padding: 15px;
@@ -36,13 +36,13 @@
             outline: none;
         }
 
-        /* Overwrite default styles of hr */
+     
         hr {
             border: 1px solid #f1f1f1;
             margin-bottom: 25px;
         }
 
-        /* Set a style for the submit button */
+      
         .registerbtn {
             background-color: #04AA6D;
             color: white;
@@ -58,13 +58,12 @@
             opacity: 1;
         }
 
-        /* Add a blue text color to links */
+
         a {
             color: dodgerblue;
         }
 
-        /* Set a grey background color and center the text of the "sign in" section */
-        .signin {
+     {
             background-color: #f1f1f1;
             text-align: center;
         }
